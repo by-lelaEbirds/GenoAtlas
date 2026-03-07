bab4ad95b3fcd949e1bca7ca9b2b2475fb5e40b3
by-lelaEbirds <matheusperescurriculum@gmail.com>
2026-03-07 18:02:40 -0300

    Update GlobeVisualizer.jsx

--- src/components/GlobeVisualizer.jsx
+++ src/components/GlobeVisualizer.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import Globe from 'react-globe.gl';
 
-export default function GlobeVisualizer() {
+export default function GlobeVisualizer({ arcsData = [] }) {
   const globeEl = useRef();
 
   useEffect(() => {
@@ -21,6 +21,12 @@ export default function GlobeVisualizer() {
         backgroundColor="rgba(0,0,0,0)"
         atmosphereColor="#00f3ff"
         atmosphereAltitude={0.15}
+        arcsData={arcsData}
+        arcColor="color"
+        arcDashLength={0.4}
+        arcDashGap={0.2}
+        arcDashAnimateTime={2000}
+        arcLabel="label"
       />
     </div>
   );
