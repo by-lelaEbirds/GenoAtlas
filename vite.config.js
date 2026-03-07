cc44e257caad660ce37e27d238afe69f119b22f4
by-lelaEbirds <matheusperescurriculum@gmail.com>
2026-03-07 17:35:18 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/GenoAtlas/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      'three/webgpu': 'three/build/three.webgpu.js',
+      'three/tsl': 'three/build/three.tsl.js'
+    }
+  }
 })
