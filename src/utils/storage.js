3b98d8c6a2515a571eae858863e729d8c7db77de
by-lelaEbirds <matheusperescurriculum@gmail.com>
2026-03-30 10:36:03 -0300

    Update storage.js

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,19 +1,19 @@
 import { Preferences } from '@capacitor/preferences';
 
 export const saveNativeData = async (key, val) => {
-  try { 
-    await Preferences.set({ key, value: val.toString() }); 
+  try {
+    await Preferences.set({ key, value: val.toString() });
   } catch (e) {
-    console.error(`GenoAtlas - Erro ao salvar ${key}:`, e);
+    console.warn('GenoAtlas - Erro ao salvar dados nativos:', e);
   }
 };
 
 export const getNativeData = async (key) => {
-  try { 
+  try {
     const { value } = await Preferences.get({ key });
     return value;
   } catch (e) {
-    console.error(`GenoAtlas - Erro ao recuperar ${key}:`, e);
+    console.warn('GenoAtlas - Erro ao buscar dados nativos:', e);
     return null;
   }
 };
