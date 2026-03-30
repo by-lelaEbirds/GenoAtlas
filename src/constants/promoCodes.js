84f0d691efd7ca00fdc9ef2c8a7d6a601355d625
by-lelaEbirds <matheusperescurriculum@gmail.com>
2026-03-29 23:49:57 -0300

    Create promoCodes.js

---
+++ src/constants/promoCodes.js
@@ -0,0 +1,15 @@
+export const PROMO_CODES = {
+  // Seu código provisório de testes (50k Moedas)
+  '741852963': { 
+    reward: 50000, 
+    type: 'coins', 
+    message: 'Código de Teste: +50.000 Moedas!' 
+  },
+  
+  // Um exemplo extra para quando você for lançar o jogo oficial
+  'GENOATLAS2026': { 
+    reward: 1000, 
+    type: 'coins', 
+    message: 'Presente de Boas-vindas: +1.000 Moedas!' 
+  }
+};
